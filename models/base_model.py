65a725e390e43b982680276ec945b26b2480c2b5
FatihSir <140240594+FatihSir@users.noreply.github.com>
2024-02-11 09:04:56 +0300

    Task 2

--- models/base_model.py
+++ models/base_model.py
@@ -1,44 +1,58 @@
 #!/usr/bin/python3
 """ this is the base_model """
 
+
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
-	""" The Base Mode3l class """
+    """ The Base Model class """
 
-	def __init__(self, *args, **kwargs):
-		"""
-		__init__ constructor method
+    def __init__(self, *args, **kwargs):
+        """
+        __init__ constructor method
+        This method recreates an instance if attributes provided
+        otherwise create a new one
 
-		Args:
-		args: takes positional arguments
-		kwargs: takes key-value arguments
-		"""
-		self.id = str(uuid.uuid4())
-		self.created_at = datetime.now
-		self.updated_at = self.created_at
+        Args:
+            args: takes positional arguments
+            kwargs: takes key-value arguments
+        """
 
-	def __str__(self):
-		"""
-		string representasion to the object
-		"""
-	
-		return("[{}] ({}) {}".format(self.__class__.__name__, self.id, self__dict__))
-		
-	def save(self):
-		"""
-		save is a method that will update current date time
-		"""
-		
-		self.update_at = datetime.now
+        if kwargs:
+            del kwargs["__class__"]
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    to_dt = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, to_dt)
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
-	def to_dict(self):
-		"""
-		returns a dictionary containing all keys/values of __dict__
-		"""
-		dict = dict(self.__dict__)
-		dict["__class__"] = self.__class__.__name
-		dict["created_at"] = dict["created_at"].isoformat()
-		dict["updated_at"] = dict["updated_at"].isoformat()
-		return dict
+    def __str__(self):
+        """
+        string representation to the object
+        """
+
+        return ("[{}] ({}) {}"
+                .format(self.__class__.__name__, self.id, self.__dict__))
+
+    def save(self):
+        """
+        save is a method that will update current date time
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        returns a dictionary containing all keys/values of __dict__
+        """
+        to_dict = self.__dict__
+        to_dict["__class__"] = self.__class__.__name__
+        to_dict["created_at"] = to_dict["created_at"].isoformat()
+        to_dict["updated_at"] = to_dict["updated_at"].isoformat()
+        return to_dict
