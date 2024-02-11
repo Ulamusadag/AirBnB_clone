053f110582f52f202168b6899483452e5f34774e
FatihSir <140240594+FatihSir@users.noreply.github.com>
2024-02-11 10:01:51 +0300

    Store first object

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@
 
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -19,19 +20,19 @@ class BaseModel:
             args: takes positional arguments
             kwargs: takes key-value arguments
         """
-
-        if kwargs:
-            del kwargs["__class__"]
+        if len(kwargs) > 0:
             for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
                 if key == "created_at" or key == "updated_at":
-                    to_dt = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, key, to_dt)
-                else:
-                    setattr(self, key, value)
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -39,20 +40,22 @@ class BaseModel:
         """
 
         return ("[{}] ({}) {}"
-                .format(self.__class__.__name__, self.id, self.__dict__))
+                .format(type(self).__name__, self.id, self.__dict__))
 
     def save(self):
         """
         save is a method that will update current date time
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
         returns a dictionary containing all keys/values of __dict__
         """
-        to_dict = self.__dict__
-        to_dict["__class__"] = self.__class__.__name__
-        to_dict["created_at"] = to_dict["created_at"].isoformat()
-        to_dict["updated_at"] = to_dict["updated_at"].isoformat()
-        return to_dict
+        dict_var = {**self.__dict__}
+        dict_var["__class__"] = type(self).__name__
+        dict_var["created_at"] = dict_var["created_at"].isoformat()
+        dict_var["updated_at"] = dict_var["updated_at"].isoformat()
+
+        return dict_var
